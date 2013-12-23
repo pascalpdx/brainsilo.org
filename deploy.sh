f7f589dd10d3d9482081be11748ef7b44ce83583
Dana Woodman <dana@danawoodman.com>
2013-12-23 09:56:33 -0800

    rebuilding site Mon Dec 23 09:56:33 PST 2013

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 hugo --source=./
 git add .
-msg="rebuilding site `date`"
-git commit -m "$msg"
+git commit -m "rebuilding site `date`"
 git push origin master
 git subtree push --prefix=public public master
