3c79beaaf7d612033fab494227f3f3843fd35237
Dana Woodman <dana@danawoodman.com>
2014-02-01 11:58:40 -0800

    copy over 404 page

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 # Build the project.
 hugo -s ./
 
+# Copy over the 404 file so Github sees it.
+cp public/404/index.html public/404.html
+
 # Add changes to git.
 git add .
 
